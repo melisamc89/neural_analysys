174db9b31c81158b988cc32dea2c33b5d2ade6aa
melisamc <melisa.mc89@gmail.com>
2022-03-21 10:31:14 +0100

    lda and updates

---
+++ src/.ipynb_checkpoints/configuration-checkpoint.py
@@ -0,0 +1,15 @@
+import os
+
+#%% ENVIRONMENT VARIABLES
+os.environ['PROJECT_DIR_LOCAL'] = '/home/melisamc/Documentos/'
+
+os.environ['DATA_DIR_LOCAL'] = '/home/melisamc/Documentos/'
+
+os.environ['LOCAL_USER'] = 'melisa'
+os.environ['ANALYST'] = 'Meli'
+
+#%% PROCESSING
+
+
+os.environ['PROJECT_DIR'] = os.environ['PROJECT_DIR_LOCAL'] 
+os.environ['DATA_DIR'] = os.environ['DATA_DIR_LOCAL'] 
--- src/configuration.py
+++ src/configuration.py
@@ -1,9 +1,9 @@
 import os
 
 #%% ENVIRONMENT VARIABLES
-os.environ['PROJECT_DIR_LOCAL'] = '/home/melisa/Documents/'
+os.environ['PROJECT_DIR_LOCAL'] = '/home/melisamc/Documentos/'
 
-os.environ['DATA_DIR_LOCAL'] = '/home/melisa/Documents/'
+os.environ['DATA_DIR_LOCAL'] = '/home/melisamc/Documentos/'
 
 os.environ['LOCAL_USER'] = 'melisa'
 os.environ['ANALYST'] = 'Meli'
